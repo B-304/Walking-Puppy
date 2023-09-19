83967e3de138893d3f5fded0c9233fed7aa849bd
imin3672 <imin3672@naver.com>
2023-09-19 16:09:55 +0900

    [feat]: dog 단건 조회 기능 구현

--- backend/src/main/java/io/ssafy/p/j9b304/backend/domain/dog/service/DogService.java
+++ backend/src/main/java/io/ssafy/p/j9b304/backend/domain/dog/service/DogService.java
@@ -2,6 +2,7 @@ package io.ssafy.p.j9b304.backend.domain.dog.service;
 
 //import io.ssafy.p.j9b304.backend.common.repository.ImageRepository;
 
+import io.ssafy.p.j9b304.backend.domain.dog.dto.DogGetResponseDto;
 import io.ssafy.p.j9b304.backend.domain.dog.dto.DogModifyRequestDto;
 import io.ssafy.p.j9b304.backend.domain.dog.entity.Dog;
 import io.ssafy.p.j9b304.backend.domain.dog.repository.DogRepository;
@@ -24,5 +25,17 @@ public class DogService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 강아지가 없습니다."));
 
         originaDog.modifyDog(dogModifyRequestDto);
+    }
+
+    public DogGetResponseDto getDog(Long dogId) {
+        // todo : 사용자가 유효한지 검증
+        Dog dog = dogRepository.findByDogId(dogId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 강아지가 없습니다."));
+
+//        Image image = imageRepository.findByImageId(dog.getDogLevelId().getImageId())
+//                .orElseThrow(() -> new IllegalArgumentException("해당 이미지가 없습니다."));
+
+//        return dog.toResponse(image.getPath());
+        return dog.toResponse();
     }
 }
