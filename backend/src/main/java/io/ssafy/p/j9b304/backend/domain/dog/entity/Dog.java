181afd4e6cf897ab90a951650a7b0f2e57a43e4a
imin3672 <imin3672@naver.com>
2023-09-19 15:53:37 +0900

    [feat]: dog 엔티티 생성

---
+++ backend/src/main/java/io/ssafy/p/j9b304/backend/domain/dog/entity/Dog.java
@@ -0,0 +1,51 @@
+package io.ssafy.p.j9b304.backend.domain.dog.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "dog")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dog_id", nullable = false)
+    private Long dogId;
+
+    @ColumnDefault("'dog'")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ColumnDefault("0")
+    @Column(name = "exp", nullable = false)
+    private int exp;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+//    @OneToOne(mappedBy = "dog")
+//    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dog_level_id")
+    private DogLevel dogLevelId;
+
+
+    @Builder
+    public Dog(Long dogId, String name, int exp, LocalDateTime createdAt/*, User user */, DogLevel dogLevelId) {
+        this.dogId = dogId;
+        this.name = name;
+        this.exp = exp;
+        this.createdAt = createdAt;
+//        this.user = user;
+        this.dogLevelId = dogLevelId;
+    }
+}
