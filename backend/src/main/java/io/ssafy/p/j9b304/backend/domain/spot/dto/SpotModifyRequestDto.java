172d13b62822880cec896c14e10f29ad033d87ce
hzim <hzim0422@gmail.com>
2023-09-20 10:55:39 +0900

    [feat]: spot 수정 기능 구현

---
+++ backend/src/main/java/io/ssafy/p/j9b304/backend/domain/spot/dto/SpotModifyRequestDto.java
@@ -0,0 +1,17 @@
+package io.ssafy.p.j9b304.backend.domain.spot.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Getter
+public class SpotModifyRequestDto {
+    @NotNull(message = "이름을 입력해주세요.")
+    @Size(min = 1)
+    private String name;
+    private boolean open;
+
+}
