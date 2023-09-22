8fffac81b825ed059a1adf15259e14374aad4073
sylee723 <seoyeonlee0723@gmail.com>
2023-09-22 09:23:10 +0900

    [feat]: 새로운 산책 경로 등록 시 산책 스팟 생성

--- backend/src/main/java/io/ssafy/p/j9b304/backend/domain/walk/dto/request/WalkAddRequestDto.java
+++ backend/src/main/java/io/ssafy/p/j9b304/backend/domain/walk/dto/request/WalkAddRequestDto.java
@@ -3,11 +3,11 @@ package io.ssafy.p.j9b304.backend.domain.walk.dto.request;
 import io.ssafy.p.j9b304.backend.domain.walk.entity.Theme;
 import io.ssafy.p.j9b304.backend.domain.walk.entity.Walk;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -24,8 +24,8 @@ public class WalkAddRequestDto {
     @Schema(description = "도착지 경도")
     BigDecimal endLongitude;
 
-//    @Schema(description = "스팟 리스트")
-//    List<Integer> spotList;
+    @Schema(description = "스팟 리스트")
+    List<Long> spotList;
 
     @Schema(description = "테마 식별번호")
     Long themeId;
@@ -33,15 +33,15 @@ public class WalkAddRequestDto {
     @Schema(description = "예상 소요시간")
     Integer estimatedTime;
 
-    @Builder
-    public WalkAddRequestDto(BigDecimal startLatitude, BigDecimal startLongitude, BigDecimal endLatitude, BigDecimal endLongitude, Long themeId, Integer estimatedTime) {
-        this.startLatitude = startLatitude;
-        this.startLongitude = startLongitude;
-        this.endLatitude = endLatitude;
-        this.endLongitude = endLongitude;
-        this.themeId = themeId;
-        this.estimatedTime = estimatedTime;
-    }
+//    @Builder
+//    public WalkAddRequestDto(BigDecimal startLatitude, BigDecimal startLongitude, BigDecimal endLatitude, BigDecimal endLongitude, Long themeId, Integer estimatedTime) {
+//        this.startLatitude = startLatitude;
+//        this.startLongitude = startLongitude;
+//        this.endLatitude = endLatitude;
+//        this.endLongitude = endLongitude;
+//        this.themeId = themeId;
+//        this.estimatedTime = estimatedTime;
+//    }
 
     public Walk toEntity(Theme theme) {
         return Walk.builder()
---
+++ backend/src/main/java/io/ssafy/p/j9b304/backend/domain/walk/repository/WalkSpotRepository.java
@@ -0,0 +1,7 @@
+package io.ssafy.p.j9b304.backend.domain.walk.repository;
+
+import io.ssafy.p.j9b304.backend.domain.walk.entity.WalkSpot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalkSpotRepository extends JpaRepository<WalkSpot, Long> {
+}
