9427975caf3a11afdea5af9256afe8cdf3c31dc1
imin3672 <imin3672@naver.com>
2023-09-27 00:40:22 +0900

    [feat]: 기능별 사용자 검증 추가

--- backend/src/main/java/io/ssafy/p/j9b304/backend/domain/walk/repository/WalkRepository.java
+++ backend/src/main/java/io/ssafy/p/j9b304/backend/domain/walk/repository/WalkRepository.java
@@ -1,10 +1,15 @@
 package io.ssafy.p.j9b304.backend.domain.walk.repository;
 
+import io.ssafy.p.j9b304.backend.domain.user.entity.User;
 import io.ssafy.p.j9b304.backend.domain.walk.entity.Walk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WalkRepository extends JpaRepository<Walk, Long> {
-    List<Walk> findByState(char state);
+
+    Optional<Walk> findByWalkIdAndUser(Long walkId, User user);
+
+    List<Walk> findByUserAndState(User walker, char c);
 }
