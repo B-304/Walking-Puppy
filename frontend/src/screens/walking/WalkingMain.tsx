d271d024b92b650ab9928aecf9d696cdf805f8e8
안도희 <adh1120@naver.com>
2023-10-04 20:10:24 +0900

    [feat] navigation

--- frontend/src/screens/walking/WalkingMain.tsx
+++ frontend/src/screens/walking/WalkingMain.tsx
@@ -21,14 +21,22 @@ const WalkingMain:React.FC = (): JSX.Element => {
   const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <Stack.Navigator initialRouteName='NewWalkingSetting'>
-<<<<<<< HEAD
-      <Stack.Screen name="NewWalkingSetting" component={NewWalkingSetting} />
-      <Stack.Screen name="산책 설정" component={WalkingSetting} />
+      <Stack.Screen name="산책 설정" component={NewWalkingSetting}         options={{
+          headerShown: true,
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontSize: 24,
+          },
+        }}/>
+      <Stack.Screen name="산책 테마 설정" component={WalkingSetting} 
+              options={{
+                headerShown: true,
+                headerTitleAlign: 'center',
+                headerTitleStyle: {
+                  fontSize: 24,
+                },
+              }}/>
       <Stack.Screen name="TimeThemeSetting" component={TimeThemeSetting}/>
-=======
-      <Stack.Screen name="NewWalkingSetting" component={NewWalkingSetting} options={{headerShown: false}}/>
-      <Stack.Screen name="TimeThemeSetting" component={TimeThemeSetting} options={{headerShown: false}}/>
->>>>>>> fb2a5e42c1e25954fae12ee7e3d7e7d446b1cb82
       <Stack.Screen name="WalkingMap" component={WalkingMap}/>
       <Stack.Screen name="StartDesMap" component={StartDesMap} options={{headerTitle:'장소 검색', headerTitleAlign:'center'}}/>
     </Stack.Navigator>
