a0e2c7bf533e97ec401f1c7ef6f29dec76f23565
안도희 <adh1120@naver.com>
2023-10-04 17:58:18 +0900

    [Feat] navigator

--- frontend/src/screens/walking/WalkingMain.tsx
+++ frontend/src/screens/walking/WalkingMain.tsx
@@ -5,12 +5,14 @@ import NewWalkingSetting from './NewWalkingSetting';
 import TimeThemeSetting from './TimeThemeSetting';
 import WalkingMap from './WalkingMap';
 import StartDesMap from './StartDesMap';
+import WalkingSetting from './WalkingSetting';
 
 const WalkingMain:React.FC = (): JSX.Element => {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator initialRouteName='NewWalkingSetting'>
       <Stack.Screen name="NewWalkingSetting" component={NewWalkingSetting} />
+      <Stack.Screen name="산책 설정" component={WalkingSetting} />
       <Stack.Screen name="TimeThemeSetting" component={TimeThemeSetting}/>
       <Stack.Screen name="WalkingMap" component={WalkingMap}/>
       <Stack.Screen name="StartDesMap" component={StartDesMap} />
