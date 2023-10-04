037a3023596c889f9e51e54125c898a51df9fc80
DESKTOP-1JVI7QI\SSAFY <asdzxc3621@hanmail.net>
2023-10-04 13:14:02 +0900

    [feat] front 컴포넌트 및 산책 경로 설정 (현재 스크랩에서 산책세팅 테스트중)

--- frontend/src/screens/myPage/MyPage.tsx
+++ frontend/src/screens/myPage/MyPage.tsx
@@ -1,10 +1,12 @@
  import { View, Text } from 'react-native'
  import React from 'react'
- 
+ import Octicons from 'react-native-vector-icons/Octicons';
+
  const MyPage:React.FC = (): JSX.Element => {
    return (
      <View>
        <Text>MyPage</Text>
+       
      </View>
    )
  }
