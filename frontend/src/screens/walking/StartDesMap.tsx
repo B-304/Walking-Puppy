6ded726dd596131c64abd1288210b2808743b593
DESKTOP-1JVI7QI\SSAFY <asdzxc3621@hanmail.net>
2023-10-04 14:55:31 +0900

    Revert "Revert "[feat] front 컴포넌트 및 산책 경로 설정 (현재 스크랩에서 산책세팅 테스트중)""
    This reverts commit 45bd2716b740d298fe1bb069a48346648434250a.

--- frontend/src/screens/walking/StartDesMap.tsx
+++ frontend/src/screens/walking/StartDesMap.tsx
@@ -1,29 +1,123 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState, useCallback } from 'react';
+import { View, TextInput, Button, FlatList, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import axios from 'axios';
+import { useNavigation,RouteProp, } from '@react-navigation/native';
+import {RootStackParamList} from './WalkingMain'
+import { StackNavigationProp } from '@react-navigation/stack';
 
-const StartDesMap: React.FC = ({ route }: any) => {
-  // type은 'start' 또는 'destination'이 될 수 있습니다.
-  const { type } = route.params;
+type LocationData = {
+  lat: number;
+  lng: number;
+  formatted_address: string;
+} | null;
+
+
+
+const StartDesMap: React.FC = ({route}) => {
+  const { setFunction } = route.params;
+  
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList, 'NewWalkingSetting'>>();
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState<LocationData>(null);
+  
+  const onChangePlace = useCallback((text:any) => setSearchTerm(text),[])
+  
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get('https://maps.googleapis.com/maps/api/place/autocomplete/json', {
+        params: {
+          input: searchTerm,
+          key: '${googleMapApiKey}'
+        }
+      });
+  
+      if (response.data.predictions) {
+        setSearchResults(response.data.predictions);
+      }
+    } catch (error) {
+      console.error('Error searching places:', error);
+    }
+  };
+
+  const selectPlace = async (placeId: string) => {
+    try {
+      const response = await axios.get('https://maps.googleapis.com/maps/api/place/details/json', {
+        params: {
+          place_id: placeId,
+          key: '${googleMapApiKey}'
+        }
+      });
+  
+      if (response.data.result) {
+        const locationData = {
+          lat: response.data.result.geometry.location.lat,
+          lng: response.data.result.geometry.location.lng,
+          formatted_address: response.data.result.formatted_address
+        };
+        setSelectedLocation(locationData);
+      }
+    } catch (error) {
+      console.error('Error fetching place details:', error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        // 다른 MapView 설정
+      <TextInput
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+        placeholder="장소 검색"
+      />
+      <Button title="검색" onPress={handleSearch} />
+      <MapView style={styles.map}>
+        {selectedLocation && (
+          <Marker
+            coordinate={{
+              latitude: selectedLocation.lat,
+              longitude: selectedLocation.lng,
+            }}
+            title={selectedLocation.formatted_address}
+          />
+        )}
+      </MapView>
+      <FlatList
+        data={searchResults}
+        keyExtractor={(item) => item.place_id}
+        renderItem={({ item }) => (
+          <Button
+            title={item.description}
+            onPress={() => selectPlace(item.place_id)}
+          />
+        )}
+      />
+      <Button
+        title="출발"
+        onPress={() => {
+          navigation.navigate('NewWalkingSetting', { startLocation: selectedLocation });
+        }}
+      />
+      <Button
+        title="도착"
+        onPress={() => {
+          navigation.navigate('NewWalkingSetting', { endLocation: selectedLocation });
+        }}
       />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    padding: 10,
   },
   map: {
     flex: 1,
-  }
+    marginBottom: 10,
+  },
 });
 
 export default StartDesMap;
